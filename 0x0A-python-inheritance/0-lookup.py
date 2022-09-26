ecbb96e39ebe9aaa0a8b283ca16e89303d0cf106
Kingyaga <99516275+Kingyaga@users.noreply.github.com>
2022-09-26 14:39:13 +0100

    Create 0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+s (5 sloc)  172 Bytes
+
+#!/usr/bin/python3
+"""Defines an object attribute lookup function."""
+
+
+def lookup(obj):
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
