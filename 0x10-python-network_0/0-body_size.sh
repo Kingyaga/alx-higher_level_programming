f0b191436d9f5b603f524dac50b727595f980892
Kingyaga <99516275+Kingyaga@users.noreply.github.com>
2024-02-18 20:01:51 +0100

    Update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
-
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <url>"
-    exit 1
-fi
-
-url=$1
-
-# Send the request using curl
-response=$(curl -s "$url")
-
-# Get the size of the response body
-size=${#response}
-
-# Display the size in bytes
-echo "$size"
+# Get the byte size of the HTTP response header for a given URL.
+curl -s "$1" | wc -c
